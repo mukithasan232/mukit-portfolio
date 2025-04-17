158990828958afb59dbe8824f5f71d71de15631d
mukithasan232 <144932194+mukithasan232@users.noreply.github.com>
2025-04-17 23:17:18 +0600

    Add files via upload

---
+++ assets/index.js
@@ -0,0 +1,4 @@
+document.querySelector("body").style.backgroundColor = "skyblue";
+document.querySelector("#hero h3").style.color = "white";
+document.querySelector("#hero h3").style.fontSize = "5rem";
+document.querySelector("button").style.textAlign = "center";
